96735be6fc1388b9e5812e0911a45414033896f6
Yang-M4Pro <leeyang1991@gmail.com>
2026-01-25 12:15:35 -0700

    add prune and passwd template

---
+++ prune.py
@@ -0,0 +1,105 @@
+from cloudreve import CloudreveV4
+from pathlib import Path
+import os
+import certifi
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+
+class Prune:
+
+    def __init__(self):
+        self.root_dir = '/_Transfer'
+
+        BASE_URL, username, password = self.get_passwd()
+        self.conn = CloudreveV4(BASE_URL)
+        self.conn.login(username, password)
+        pass
+
+    def get_passwd(self):
+        CONFIG_FILE = Path.home() / ".config" / "cloudreve" / "passwd"
+        login_info = open(CONFIG_FILE, 'r')
+
+        login_info = login_info.read().splitlines()
+        BASE_URL = login_info[0]
+        username = login_info[1]
+        password = login_info[2]
+
+        return BASE_URL, username, password
+
+    def get_url(self, remote_fname):
+        data = self.conn.get_info(remote_fname)
+        source_link_str = self.conn.get_source_url(remote_fname)
+        return source_link_str
+
+
+    def tree(self, remote_path):
+        dir_info_dict = self.conn.list(remote_path)
+        files_info_dict = dir_info_dict['files']
+        path_list = []
+        for dict_i in files_info_dict:
+            path_origin = dict_i['path']
+            path = path_origin.replace('cloudreve://my', '')
+            f_type = dict_i['type']
+            if f_type == 0: # file
+                path_list.append(path)
+            elif f_type == 1: # folder
+                path_list_i = self.tree(path)
+                path_list += path_list_i
+            else:
+                raise Exception('unknown file type')
+        return path_list
+
+    def get_dir_files(self, remote_path):
+        dir_info_dict = self.conn.list(remote_path)
+        files_info_dict = dir_info_dict['files']
+        path_list = []
+        for dict_i in files_info_dict:
+            # pprint(dict_i)
+            path = dict_i['path']
+            path = path.replace('cloudreve://my', '')
+            path_list.append(path)
+        return path_list
+
+
+    def check_is_file(self, remote_path):
+        info = self.conn.get_info(remote_path)
+        f_type = info['type']
+        if f_type == 0: return True
+        elif f_type == 1: return False
+        else: raise Exception('unknown file type')
+
+
+    def check_is_exists(self,remote_path):
+        try:
+            self.check_is_file(remote_path)
+            return True
+        except:
+            return False
+
+    def delete(self,remote_d):
+        is_exist = self.check_is_exists(remote_d)
+        if is_exist:
+            try:
+                self.conn.delete(remote_d)
+                return True
+            except:
+                return False
+        else:
+            return True
+
+def main():
+    P = Prune()
+    flist = P.get_dir_files(P.root_dir)
+    for fpath in flist:
+        print('deleting',fpath)
+        success = P.delete(fpath)
+        if success:
+            print('success')
+        else:
+            print('failed')
+
+    pass
+
+if __name__ == '__main__':
+    main()
